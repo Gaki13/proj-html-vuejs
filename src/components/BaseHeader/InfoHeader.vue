e3b7e7a6884f78735154b5acb4f5cdaa2ad4ce4a
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-15 16:41:52 +0200

    aggiunta parte info del footer

--- src/components/BaseHeader/InfoHeader.vue
+++ src/components/BaseHeader/InfoHeader.vue
@@ -1,6 +1,19 @@
 <template>
-    <div class="info-header">
-
+    <div class="info-header ">
+        <div class="container-st">
+            <div class="row justify-content-between">
+                <div class="col-6 d-flex align-items-center">
+                    <p><font-awesome-icon icon="fa-solid fa-clock" /> Open Hours: Mon - Sat - 9:00 -18:00</p>
+                </div>
+                <div class="col-4 d-flex align-items-center justify-content-end justify-content-between">
+                    <span><font-awesome-icon icon="fa-solid fa-phone" /> +1 (305) 1234-5678</span>
+                    <span><font-awesome-icon icon="fa-solid fa-envelope" /> hello@example.com</span>
+                    <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                    <font-awesome-icon icon="fa-brands fa-twitter" />
+                    <font-awesome-icon icon="fa-brands fa-linkedin" />
+                </div>
+            </div>
+        </div>              
     </div>
 </template>
 
@@ -11,5 +24,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .info-header{
+        height: 3.125rem;
+        background-color: #21333e;
+        color: #c0c3d0;
+        .row{
+            height: 3.125rem;
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+/*Bootstrap*/
 import 'bootstrap/dist/css/bootstrap.css'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+/* add icons to the library */
+library.add(faClock, faPhone, faEnvelope, faTwitter, faFacebookF, faLinkedin)
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
