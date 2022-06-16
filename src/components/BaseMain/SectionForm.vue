df70b66b15d4761c6ce28c2c7a92ee48380487f0
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-17 00:56:08 +0200

    inizio sezione form

--- src/components/BaseMain/SectionForm.vue
+++ src/components/BaseMain/SectionForm.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="section">
+    <div class="container-st">
+      <div class="row">
+        <!-- sezione del form -->
+        <div class="col-8">
+          <!-- titolo della sezione -->
+          <p class="upper-title">Send a message</p>
+          <div class="section-title">
+            <h2>Get in</h2>
+            <h2 class="boxed-title">Touch</h2>
+          </div>
+          <p>We will respond to your message as soon as posible</p>
+          <!-- form vero e proprio -->
+          
+        </div>
+        <!-- sezione aside con le info -->
+        <div class="col-4">
 
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +30,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .container-st{
+    .boxed-title{
+      margin-left: .3125rem;
+    }
+  }
 </style>
