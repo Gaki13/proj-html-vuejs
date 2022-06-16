7355cff09ac3a11b01d472890d8c66853e9ed527
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-16 19:09:18 +0200

    aggiunta section Transport

--- src/components/BaseMain/SectionTransport.vue
+++ src/components/BaseMain/SectionTransport.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="section">
-    <div class="container-st mt-5">
+    <div class="container-st ">
         <div class="row">
             <!-- Parte con titolo e testo -->
             <div class="col-9">
                 <p class="upper-title">who we are</p>
-                <h2><font-awesome-icon icon="fa-regular fa-gem" />Excellence in Transport</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias at aliquam distinctio amet voluptate maiores nostrum dignissimos tenetur! Eos eligendi ducimus porro facere architecto debitis maiores nihil fuga earum perferendis.</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptate voluptas. Distinctio deleniti laborum necessitatibus atque? Exercitationem sed, delectus, a obcaecati libero labore dolorem praesentium ipsa quidem impedit iste nisi?</p>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus aperiam amet alias veritatis sunt repudiandae, minus atque exercitationem incidunt vel excepturi id esse deserunt quaerat quisquam iusto suscipit natus quod.</p>
+               <!-- titolo della sezione -->
+                <div class="section-title">
+                    <h2 class="boxed-title"><font-awesome-icon icon="fa-regular fa-gem" />Excellence </h2>
+                     <h2>in Transport</h2> 
                 </div>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, maiores fuga? </p>
-                <ul>
-                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
-                    <li> Dolore qui, doloribus ab, voluptate possimus ipsa </li>
-                    <li>Perferendis quas praesentium exercitationem deserunt</li>
-                    <li>Error corrupti fugiat pariatur eum numquam dolorem</li>
-                    <li> Consectetur harum, adipisci velit quaerat ullam ea </li>
-                </ul>
+                <!-- corpo della sezione -->
+                <div class="section-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias at aliquam distinctio amet voluptate maiores nostrum dignissimos tenetur! Eos eligendi ducimus porro facere architecto debitis maiores nihil fuga earum perferendis.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptate voluptas. Distinctio deleniti laborum necessitatibus atque? Exercitationem sed, delectus, a obcaecati libero labore dolorem praesentium ipsa quidem impedit iste nisi?</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus aperiam amet alias veritatis sunt repudiandae, minus atque exercitationem incidunt vel excepturi id esse deserunt quaerat quisquam iusto suscipit natus quod.</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, maiores fuga? </p>
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li> Dolore qui, doloribus ab, voluptate possimus ipsa </li>
+                        <li>Perferendis quas praesentium exercitationem deserunt</li>
+                        <li>Error corrupti fugiat pariatur eum numquam dolorem</li>
+                        <li> Consectetur harum, adipisci velit quaerat ullam ea </li>
+                    </ul>
+                </div>
+                
             </div>
             <!-- Parte con elenco dei camion -->
             <div class="col-3 d-flex flex-column">
@@ -62,6 +70,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+   .container-st{
+    margin-top: 8.125rem;
+    margin-bottom: 10rem;
+   }
+   
+   // style del corpo
+    .section-body{
+        color: #656d75; 
+        font-size: 16px;
+        div{
+            border-left: 3px solid #daeced;
+            padding: .5rem 1rem;
+            font-style: italic;
+            margin: 1rem 0;
+        }
+        ul{
+            padding-left: 16px ;
+            li{
+                margin: .5rem 0;
+            }
+        }
+    }
+    // style dell'aside
     .vehicle-card{   
         display: flex;
         gap: .625rem;
