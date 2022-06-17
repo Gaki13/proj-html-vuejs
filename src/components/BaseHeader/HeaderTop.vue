7c7c1929f349b1cf4dbf288091f0253f08f0e9c5
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-17 15:07:07 +0200

    stampa dinamica link nav

--- src/components/BaseHeader/HeaderTop.vue
+++ src/components/BaseHeader/HeaderTop.vue
@@ -2,15 +2,11 @@
   <div class="header-top">
     <div class="container-st">
       <div class="row py-3">
-        <div class="col 2"><img src="../../assets/img/logo-1.png" alt=""></div>
+        <div class="col 2"><img src="../../assets/img/logoheader.png" alt=""></div>
         <div class="col-9">
           <nav>
             <ul class="d-flex justify-content-between align-items-center">
-              <li>HOME</li>
-              <li>ABOUT</li>
-              <li>SERVICES</li>
-              <li>PROCESS</li>
-              <li>TESTIMONIALS</li>
+              <li v-for="link in links" :key="link">{{link.text}}</li>
               <li><font-awesome-icon icon="fa-regular fa-user" /></li>
               <li><button class="primary-button">GET IN TOUCH</button></li>
             </ul>
@@ -23,7 +19,39 @@
 
 <script>
 export default {
-    name: 'HeaderTop'
+  name: 'HeaderTop',
+  data(){
+    return{
+      links: [
+        {
+          text: "Home",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "About",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Sevices",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Process",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Testimonials",
+          url: "#",
+          current: false,
+        },
+      ],
+    }
+  }
+  
 }
 </script>
 
@@ -31,9 +59,15 @@ export default {
   .header-top{
     color: white;
     font-weight: bold;
+    img{
+      width: 50%;
+    }
     ul{
       list-style: none;
       font-size: 1.125rem;
+      li{
+        text-transform: uppercase;
+      }
     }
   }
 </style>
